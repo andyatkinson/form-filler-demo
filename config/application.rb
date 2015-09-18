f3f7e3f51768287a65ec1efd6ed213a02f267d66
Andy Atkinson <andyatkinson@gmail.com>
2015-09-18 16:36:42 -0500

    That did not work

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,5 @@ module FormFillerDemo
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.assets.enabled = false
   end
 end
